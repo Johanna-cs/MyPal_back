1f0ed81404b5dfeb4306e4e49c369cea4b8f20c4
johanna-cs <johanna.cas@icloud.com>
2020-10-04 09:22:00 +0200

    changing listenning port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const models = require('./models')
 const app = express()
-const port = 4000
+const port = 4001
 const userRouter = require('./routes/userRouter')
 
 const cors = require('cors');
